40d31b86f7b556646eb768daee1e26530fa97443
Anita Safar <ancsa0416@gmail.com>
2021-10-13 13:54:20 +0100

    3rd test done

--- src/python_functions_practice.py
+++ src/python_functions_practice.py
@@ -4,3 +4,7 @@ def return_10():
 def add(first_number, second_number):
     sum = first_number + second_number
     return sum
+
+def subtract(first_number, second_number):
+    sum = first_number - second_number
+    return sum
