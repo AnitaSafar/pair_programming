60e06b89d6e10a74820caf7b9f0cb8bf859af897
Anita Safar <ancsa0416@gmail.com>
2021-10-13 14:23:13 +0100

    9th test done

--- src/python_functions_practice.py
+++ src/python_functions_practice.py
@@ -26,7 +26,24 @@ def join_string( string_1, string_2 ):
 def add_string_as_number(string_1, string_2):
     return int(string_1) + int(string_2)
 
+months = {
+    1: "January",
+    2: "February",
+    3: "March",
+    4: "April",
+    5: "May",
+    6: "June",
+    7: "July",
+    8: "August",
+    9: "September",
+    10: "October",
+    11: "November",
+    12: "December"
+}
 
+
+def number_to_full_month_name(first_month):
+    return(months[1])
     
 
     
